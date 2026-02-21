45da29a2354b12cec375f63e37b46d90ff1bda99
Orphio George <nguyennnganas146@gmail.com>
2026-02-21 22:58:57 +0700

    update

---
+++ sitecustomize.py
@@ -0,0 +1,12 @@
+import os
+import warnings
+import logging
+
+# Tắt warning toàn bộ Python
+warnings.filterwarnings("ignore")
+
+# Tắt telemetry (Chroma, etc.)
+os.environ["ANONYMIZED_TELEMETRY"] = "False"
+
+# Tắt log rác
+logging.getLogger().setLevel(logging.ERROR)
